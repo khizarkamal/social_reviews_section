823aaa0f25d53cf32eaf70afb2f35dafa6b84432
Khizar Kamal <khizarkamal0321@gmail.com>
2024-01-27 17:00:01 +0500

    fix: Addes key attribute to jsx element

--- src/components/StarsCard/index.tsx
+++ src/components/StarsCard/index.tsx
@@ -12,8 +12,8 @@ const ReviewCard = (props: ReviewCardProps) => {
   return (
     <div className="flex justify-center items-center p-4 flex-col gap-4 rounded-md bg-neutral-light_grayish_magenta md:flex-row md:items-center md:even:translate-x-8 md:last:translate-x-16">
       <div className="flex flex-row gap-2">
-        {Array.from({ length: starsCount }).map((item: any) => {
-          return <Star></Star>;
+        {Array.from({ length: starsCount }).map((item: any, index) => {
+          return <Star key={index}></Star>;
         })}
       </div>
       <p className="font-bold text-primary-dark_magenta">{review}</p>
